0157751a69819f1fe9c0997190c27f733470eeef
aritrac1984 <87344726+aritrac1984@users.noreply.github.com>
2025-07-14 20:52:52 +0530

    Delete Python directory

--- Python/binary_to decimal.py
+++
@@ -1,5 +0,0 @@
-binary_num = input("Enter binary number: ")
-
-decimal_num = int(binary_num, 2)
-
-print(f"Decimal number is: {decimal_num}")
--- Python/decimal_to_binary.py
+++
@@ -1,5 +0,0 @@
-decimal_num = int(input("Enter decimal number: "))
-
-binary_num = bin(decimal_num)[2:]
-
-print(f"Binary number is: {binary_num}")
